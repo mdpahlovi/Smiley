8a151558d173f435f679beb38acbc0e65031d423
MD Pahlovi <108403206+mdpahlovi@users.noreply.github.com>
2024-04-30 10:05:56 +0600

    [Done] Project Setup

---
+++ src/app/theme.ts
@@ -0,0 +1,14 @@
+import { ThemeConfig } from "antd";
+
+const theme: ThemeConfig = {
+    token: {
+        // Seed Token
+        colorPrimary: "#00b96b",
+        borderRadius: 2,
+
+        // Alias Token
+        colorBgContainer: "#f6ffed",
+    },
+};
+
+export default theme;
